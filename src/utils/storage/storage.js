49952474933110e72015b67f590bcbe9bd8c766f
Zach Dolph <zachdolph26@gmail.com>
2021-10-19 13:31:08 -0700

    Building the contract interfaces and Vite client related functionality

---
+++ src/utils/storage/storage.js
@@ -0,0 +1,31 @@
+export const CUSTOM_NET_MAP = 'CUSTOM_NET_MAP';
+export const storageKeyMap = {
+    'mnemonics': 'vite_soliditypp_mnemonics',
+    'enableVc': 'vite_soliditypp_enableVc',
+    'CUSTOM_NET_MAP':'CUSTOM_NET_MAP'
+};
+
+export const get = key => {
+    if (!storageKeyMap[key]) {
+        throw new Error(`Can't find the storage key: ${key}`);
+    }
+
+    let str = localStorage.getItem(storageKeyMap[key]);
+    let json;
+    if (str == null) {
+        return str;
+    }
+    try {
+        json = JSON.parse(str);
+    } catch (err) {
+        console.log(err);
+    }
+    return json;
+};
+
+export const set = (key, value) => {
+    if (!storageKeyMap[key]) {
+        throw new Error(`Can't find the storage key: ${key}`);
+    }
+    localStorage.setItem(storageKeyMap[key], JSON.stringify(value));
+};
